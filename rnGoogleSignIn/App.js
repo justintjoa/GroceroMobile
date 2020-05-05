e371783b2666769281219818cee83793e5509af6
justintjoa-portfolio <justintoja@gmail.com>
2020-05-04 22:24:26 -0700

    sdf

--- rnGoogleSignIn/App.js
+++ rnGoogleSignIn/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import {View,Text} from 'react-native';
 import {Login} from "./screens/Login"
 
+
+
 const App = () => (
   <View>
     <Text>Hello World</Text>
+    <Login> poop</Login>
   </View>
 );
 
